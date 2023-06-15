56bd6648125193895acb271c94f55eb5eede2670
hamdali12 <zoroloufi1qgmail.com>
2023-06-15 08:49:40 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ *Return: 0 on successful execution
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
