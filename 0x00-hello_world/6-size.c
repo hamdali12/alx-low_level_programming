24ca460dd00aa9732127e341c892b40aaf8ca58e
hamdali12 <zoroloufi1qgmail.com>
2023-06-15 09:02:13 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ *main - Entry point of the program
+ *
+ *Return: 0 on successful execution
+ */
+int main() {
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of double: %lu byte(s)\n", sizeof(double));
+printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+printf("Size of pointer: %lu byte(s)\n", sizeof(void*));
+
+return 0;
+}
