07b29d1737917a181c9e1523fb9b95fb8bcd8012
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-06-20 22:29:04 +0100

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
 #include "main.h"
 
-/**
- * main - Entry point of the program
- *
- * Return: 0 on successful execution
- */
-int main(void)
-{
-	print_alphabet();
-    	return 0;
-}
-
 /*
 print alphabet function
 */
