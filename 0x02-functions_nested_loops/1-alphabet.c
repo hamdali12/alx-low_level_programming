96815f5fa40550c43b531107e79ace23ce0fa67b
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-06-20 21:54:22 +0100

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,22 @@
  *
  * Return: 0 on successful execution
  */
+int main(void)
+{
+    print_alphabet();
+}
+
+/*
+print alphabet function
+*/
 void print_alphabet(void)
 {
-		char letter = 'a';
+	char letter = 'a';
 
-		while (letter <= 'z')
-		{
-				_putchar(letter);
-				letter++;
-		}
-		_putchar('\n');
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
