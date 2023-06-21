3e13d820b499ce36d77dbaf27f13a867e9e06ef0
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-06-21 01:45:07 +0100

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-    for (int i = 0; i < 10; i++)
+    	for (int i = 0; i < 10; i++)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
 		{
-				char letter = 'a';
+		    _putchar(letter);
+		    letter++;
+		}
+		
+		_putchar('\n');
+    	}
 
-        while (letter <= 'z')
-				{
-            _putchar(letter);
-            letter++;
-        }
-    }
-
-    _putchar('\n');
+    
 }
