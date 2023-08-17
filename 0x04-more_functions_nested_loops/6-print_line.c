f47d713667d3857c54f5239dd74f4bac3c1116c9
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-08-17 20:44:17 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,14 @@ void print_line(int n)
 	int i;
 	if (n <= 0)
 	{
-		_putchar('\n'); // Print a newline for n <= 0
+		_putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('_'); // Print '_' n times
+			_putchar('_');
 		}
-		_putchar('\n'); // Print a newline after the line
+		_putchar('\n');
 	}
 }
