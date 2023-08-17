2607d3e261fd20a207cecc71fa5a01837f92b6c8
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-08-17 20:45:58 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * print_line - Draws a straight line of underscores
+ * @n: The number of times the character '_' should be printed
+ */
 void print_line(int n)
 {
 	int i;
