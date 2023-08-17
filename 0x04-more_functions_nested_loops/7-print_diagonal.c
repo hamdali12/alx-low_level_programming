71d1fd70f0fb5c23a402754ef02e4f2a36e8d9de
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-08-17 20:57:52 +0100

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line of backslashes
+ * @n: The number of times the character '\' should be printed
+ */
+void print_diagonal(int n)
+{ 
+	int i,j;
+
+	if (n <= 0)
+	{
+		_putchar('\n'); // Print a newline for n <= 0
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				_putchar(' '); // Print spaces before each backslash
+			}
+			_putchar('\\'); // Print a backslash for each row
+			_putchar('\n'); // Print a newline after each row
+		}
+	}
+}
+
+
