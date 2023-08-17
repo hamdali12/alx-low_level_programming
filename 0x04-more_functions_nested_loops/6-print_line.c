ccd6d61c380f00d40ca1acb5fa782cf5c3188086
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-08-17 20:30:41 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,16 @@
  * @n: The number of times the character '_' should be printed
  */
 void print_line(int n)
-{
+{	
+	int i;
+	
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
