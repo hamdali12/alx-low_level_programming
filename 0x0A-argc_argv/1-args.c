e589ab0566a545c7d0b37503c10e8155a047f879
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-02 20:49:08 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("Number of arguments: %d\n", argc - 1);
+	printf("%d\n", argc - 1);
 
 	for (i = 1; i < argc; i++)
 	{
-		printf("Argument %d: %s\n", i, argv[i]);
+		printf("%s\n", i, argv[i]);
 	}
 
 	return (0);
