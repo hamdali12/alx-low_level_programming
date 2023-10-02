45e38bb136124ae6bf4da8d3ce2456492b02816e
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-02 18:46:07 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc > 0 && argv[0] != NULL)
-    {
-        printf("%s\n", argv[0]);
-    }
-    return 0;
+	if (argc > 0 && argv[0] != NULL)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return 0;
 }
