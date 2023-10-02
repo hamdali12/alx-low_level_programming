5fc6bcfa2a6dac925fa532a9080f5f0b24293705
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-02 18:47:57 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (unused)
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0 && argv[0] != NULL)
