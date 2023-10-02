bc177f2ba8f26dc9d968aaa28ac25d8d2a6fc9d0
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-02 20:57:53 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,16 +8,9 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc)
 {
-	int i;
-
 	printf("%d\n", argc - 1);
-
-	for (i = 1; i < argc; i++)
-	{
-		printf("%d:\n", i);
-	}
 
 	return (0);
 }
