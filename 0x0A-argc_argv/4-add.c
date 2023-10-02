db4304db5a33a873da3ed1ff12e4bc842b7270bb
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-02 20:04:10 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool is_positive_number(const char *str)
+{
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (false);
+		}
+	}
+	return (true);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	int sum = 0;
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (!is_positive_number(argv[i]))
+		{
+    		printf("Error\n");
+    		return (1);
+    	}
+
+    	int num = atoi(argv[i]);
+
+    	if (num < 0)
+		{
+			continue;
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
