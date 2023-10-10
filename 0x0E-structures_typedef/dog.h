40b4fe09e25ace92268af4acb1948317fa87bef6
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-10 18:44:07 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,16 @@ struct dog
 	
 };
 
+/**
+ * init_dog - Initializes a struct dog.
+ * @d: Pointer to the struct dog to be initialized.
+ * @name: Pointer to a string representing the dog's name.
+ * @age: The age of the dog as a float.
+ * @owner: Pointer to a string representing the dog's owner.
+ *
+ * Description: This function initializes a struct dog with the provided
+ * values for name, age, and owner.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
