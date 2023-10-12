3dc38b2ab6bb26fe16cae9efbc35f8a699cef363
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-12 01:31:26 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -10,11 +11,16 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
+	int num1;
+	int num2;
+	int (*func)(int, int)
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
 	char *operator = argv[2];
 
-	int (*func)(int, int) = get_op_func(operator);
+	func = get_op_func(operator);
 
 	int result = func(num1, num2);
 
