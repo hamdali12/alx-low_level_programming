ea983879834b4a7b0734628935a5a0fd12fb73a8
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-24 16:11:02 +0100

    Create lists.h

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: Integer value to store in the node
+ * @next: Pointer to the next node in the list
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
